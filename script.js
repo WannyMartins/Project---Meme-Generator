38c91df57f2fb51f9ab808c1345fb1e50293f7e4
Wanny Martins <wcatery@gmail.com>
2022-02-07 15:20:16 -0300

    requisito 6

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const input = document.getElementById('text-input');
 const div = document.getElementById('meme-image-container');
 const memeInsert = document.getElementById('meme-insert');
 const memeImg = document.getElementById('meme-image');
+const bntFire = document.getElementById('fire');
+const bntWater = document.getElementById('water');
+const bntEarth = document.getElementById('earth');
 
 function texto() {
     textoInput.innerHTML = input.value;
@@ -15,6 +18,21 @@ function uploadImg(event) {
     memeImg.src = URL.createObjectURL(event.target.files[0]);
 }
 memeInsert.addEventListener('change', uploadImg);
+
+function mudaBorda(borda) {
+    div.style.border = borda;
+
+}
+bntFire.addEventListener('click', function () {
+    mudaBorda('3px dashed red')
+});
+bntEarth.addEventListener('click', function () {
+    mudaBorda('6px groove green');
+});
+bntWater.addEventListener('click', function () {
+    mudaBorda('5px double blue');
+});
+
 
 // change garante que será alterado o file de acordo com a função uploadImg;
 
