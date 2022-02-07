bbf2744b175a3301eacc22f801296285c0d07271
Wanny Martins <wcatery@gmail.com>
2022-02-07 13:57:40 -0300

    requisito 2

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const div = document.getElementById('meme-image-container');
 const memeInsert = document.getElementById('meme-insert');
 const memeImg = document.getElementById('meme-image');
 
-function texto(event){
-    textoInput.innerHTML = input.value
+function texto() {
+    textoInput.innerHTML = input.value;
 }
 
-input.addEventListener('input', texto)
+input.addEventListener('input', texto);
 
-/*function uploadImg(){
-    memeImg.src= 'image/*'
 
+function uploadImg(event) {
+    memeImg.src = URL.createObjectURL(event.target.files[0]);
 }
+memeInsert.addEventListener('change', uploadImg);
 
-memeInsert.addEventListener('click', uploadImg)*/
+// change garante que será alterado o file de acordo com a função uploadImg;
 
+// pega o objeto no memeInsert(event target) e poe como src'url' memeImg parecendo o objetoURL criado.
